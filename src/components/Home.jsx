3afd81cd38b442a11219db666b17b53cffe5ef2c
Joel <joelsammathew@gmail.com>
2024-02-04 18:48:52 +0530

    added art

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Dp from "../assets/dp.png";
 import Typewriter from "typewriter-effect";
+import Art from "./Art";
 
 const Home = () => {
+  const [art, setArt] = useState(false);
+  const switchHandler = () => {
+    setArt(~art);
+  };
   return (
     <div name="home" className="h-screen w-full">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center md:justify-between h-full px-10 md:flex-row">
@@ -27,12 +32,27 @@ const Home = () => {
             />
           </div>
         </div>
-        <div className="">
-          <img
-            src={Dp}
-            alt="my profile"
-            className="mx-auto w-2/3 md:w-full rounded-3xl"
-          />
+        <div className="flex flex-col">
+          <div className="w-full md:mt-20">
+            {art ? (
+              <Art />
+            ) : (
+              <img
+                src={Dp}
+                alt="my profile"
+                className="mx-auto md:w-full rounded-3xl p-1"
+              />
+            )}
+          </div>
+          <span></span>
+          <div
+            className="text-gray-400 m-auto cursor-pointer duration-300 text-5xl items-center hover:text-green-200"
+            onClick={switchHandler}
+            style={{
+              transform: `rotate(${art ? "-180deg" : "0"})`,
+            }}>
+            &#8594;
+          </div>
         </div>
       </div>
     </div>
